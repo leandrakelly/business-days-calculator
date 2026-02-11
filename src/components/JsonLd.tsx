e24fa7a6db2b3f7e3a47eccf5f8ccb9f6ef05a19
leandrakelly <leandrakell@gmail.com>
2026-02-10 22:51:39 -0300

    chore: improve configs

---
+++ src/components/JsonLd.tsx
@@ -0,0 +1,26 @@
+export default function JsonLd() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebApplication",
+    name: "Calculadora de Dias Úteis",
+    url: "https://diasuteis.com.br", // TODO: change later
+    description:
+      "Ferramenta para calcular datas finais somando dias úteis e ignorando feriados.",
+    applicationCategory: "BusinessApplication",
+    operatingSystem: "Any",
+    offers: {
+      "@type": "Offer",
+      price: "0",
+      priceCurrency: "BRL",
+    },
+    featureList:
+      "Cálculo de dias úteis, Feriados Nacionais, Feriados Municipais",
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  );
+}
